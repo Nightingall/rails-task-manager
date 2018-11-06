29e29e003dddb319c6dedb0a2fef97f4e92087c7
Harry Nightingall <nightingallh@gmail.com>
2018-11-06 12:26:33 +0000

    tasks created with tickbox and dynamic page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:new', to: 'tasks#create'
+  post 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
