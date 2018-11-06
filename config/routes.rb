b53c647275807d8c92c7a83883ecdf3a2d610966
Harry Nightingall <nightingallh@gmail.com>
2018-11-06 15:00:57 +0000

    all functionality working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :task_new
   get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:new', to: 'tasks#create'
-  post 'tasks/:id/edit', to: 'tasks#edit'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
